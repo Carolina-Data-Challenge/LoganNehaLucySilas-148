6f59af155dd5bce5c0873f54ba66bb390eeda418
silas <silasness1@gmail.com>
2020-10-03 16:54:53 -0400

    reorganizing and added climate data

--- scripts/blah.R
+++ scripts/blah.R
@@ -1,14 +1,14 @@
-# blah blah
-# blah
-# This is my change
 
+#loading packages
 library(tidyverse)
 library(janitor)
 library(lubridate)
 
+#reading in csvs
 durham <- read_csv("data/OpenAQDurham.csv") %>% clean_names()
 sanFran <- read_csv("data/OpenAQSanFran.csv") %>% clean_names()
-
+sanClimate <- read_csv('data/2303976.csv')
+#summarizing sanfran averages-----
 unique(sanFran$parameter)
 sanFran %>% 
   select(location, city, country, utc, local, parameter, value, unit, latitude, longitude) %>%
@@ -18,15 +18,27 @@ sanFran %>%
          day = day(utc)) %>%
   group_by( latitude, longitude) %>%
   summarize(avg_pm25 = mean(value)) -> summarySan
-  
+
+#looking at pm25 only------
+durham25 <- durham %>%
+  filter(parameter == 'pm25')
+sanfran25 <- sanFran %>%
+  filter(parameter == "pm25")
+
+ggplot(data = sanfran25, mapping = aes(x = utc, y = value))+
+  geom_point()+
+  geom_smooth(span = .1)+
+  facet_wrap(~location)
+
+#showing durham and sanfran pm25 time correlation-----
+ggplot(data = durham25, mapping = aes(x = utc, y = value))+
+  geom_point()+
+  geom_smooth(span = .1)+
+  facet_wrap(~location)
   
 ggplot(data = summarySan, mapping = aes(x = longitude, y = latitude, color = avg_pm25))+
   geom_point()+
+  geom_smooth(span = .2)+
   facet_wrap(~month)+
   geom_sf()
 
-unique(durham$location)
-unique(sanFran$location)
-
-time_durham_pm10 <- durham_pm10$utc
-month_durham_pm10 <- month(time_durham_pm10)
