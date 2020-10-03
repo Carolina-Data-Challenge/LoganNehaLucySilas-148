fed732219dfc33493201478296337d40a68f656a
nehavm123 <72309426+nehavm123@users.noreply.github.com>
2020-10-03 12:18:16 -0500

    Update blah.R

--- scripts/blah.R
+++ scripts/blah.R
@@ -7,3 +7,10 @@ library(janitor)
 
 durham <- read_csv("data/OpenAQDurham.csv") %>% clean_names()
 sanFran <- read_csv("data/OpenAQSanFran.csv") %>% clean_names()
+
+durham_pm10 <- durham %>% 
+  select(location, city, country, utc, local, parameter, value, unit) %>%
+  filter(parameter == "pm10")
+
+time_durham_pm10 <- durham_pm10$utc
+month_durham_pm10 <- month(time_durham_pm10)
