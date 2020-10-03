dc75b6d77b01b667c97c2020140fee5c1217ce83
silas <silasness1@gmail.com>
2020-10-03 11:49:03 -0400

    adding csv read to newbranch

--- scripts/blah.R
+++ scripts/blah.R
@@ -1,4 +1,9 @@
-blah blah
-blah
+# blah blah
+# blah
+# This is my change
 
-This is my change
+library(tidyverse)
+library(janitor)
+
+durham <- read_csv("data/OpenAQDurham.csv") %>% clean_names()
+sanFran <- read_csv("data/OpenAQSanFran.csv") %>% clean_names()
